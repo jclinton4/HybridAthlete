1f87df995694503ae3384d259e545cfd946890e2
unknown <joshuac@ami.com>
2024-01-25 08:39:05 -0500

    feat: enable new workouts display
    Add the ability to add new workout movements and save them
    to local storage. Display the workouts as they are added.

--- newworkout.js
+++ newworkout.js
@@ -1,44 +1,88 @@
 const addMovementButton = document.getElementById("addMovement");
-const output = document.getElementById('workoutList');
+const workoutContainer = document.getElementById('workoutList');
 const movementName = document.getElementById('movementName');
-const sets = document.getElementById('sets');
-const reps = document.getElementById('reps');
-const weight = document.getElementById('weight');
+const setsInput = document.getElementById('sets');
+const repsInput = document.getElementById('reps');
+const weightInput = document.getElementById('weight');
 const clearWorkout = document.getElementById('clear');
 
-const workouts = [];
+const workouts = JSON.parse(localStorage.getItem("workouts")) || [];
+let currentWorkout = {};
 
 const clearForm = () => {
     console.log("Entered Clearform")
-    workout = [];
-    output.innerHTML = '';
-    output.classList.add('hide');
+    workouts.splice(0,workouts.length);
+    currentWorkout = {};
+    localStorage.removeItem("workouts");
+    workoutContainer.innerHTML = "";
 };
 
-addMovementButton.addEventListener("click", (e) => {
-    e.preventDefault();
+const addOrUpdateWorkout = () => {
+    console.log(workouts);
+    console.log(currentWorkout);
+    const workoutArrIndex = workouts.findIndex((item) => item.id === currentWorkout.id);
     const workoutObj = {
         id: `${movementName.value.split(" ").join("-")}-${Date.now()}`,
         exercise: movementName.value,
-        sets: sets.value,
-        reps: reps.value,
-        weight: weight.value
+        sets: setsInput.value,
+        reps: repsInput.value,
+        weight: weightInput.value
+    };
+
+    if (workoutArrIndex === -1) {
+        workouts.unshift(workoutObj);
+    } else {
+        workouts[workoutArrIndex] = workoutObj;
     }
 
-    workouts.unshift(workoutObj);
+    localStorage.setItem("workouts", JSON.stringify(workouts));
+    updateWorkoutContainer();
+};
 
-    workouts.forEach(({id, exercise, sets, reps, weight}) => {
-        (output.innerHTML += `
-            <div class="workout" id="${id}">
-                <p><strong>Exercise: </strong>${exercise}</p>
-                <p><strong>Sets: </strong>${sets}</p>
-                <p><strong>Reps: </strong>${reps}</p>
-                <p><strong>Weight: </strong>${weight}</p>
-                <button type="button" class="btn">Edit</button>
-                <button type="button" class="btn">Delete</button>
+const updateWorkoutContainer = () => {
+    workoutContainer.innerHTML = (`
+    <div>
+        <button onclick="clearForm(this)" type="button" class="btn">Delete All</button>
+    </div>
+    `);
+
+    workouts.forEach(
+        ({id, exercise, sets, reps, weight}) => {
+            (workoutContainer.innerHTML += `
+            <div class="task" id="${id}">
+                <p><strong>Movement:</strong> ${exercise}</p>
+                <p><strong>Sets:</strong> ${sets}</p>
+                <p><strong>Reps:</strong> ${reps}</p>
+                <p><strong>Weight:</strong> ${weight}</p>
+                <button onclick="editWorkout(this)" type="button" class="btn">Edit</button>
+                <button onclick="deleteWorkout(this)" type="button" class="btn">Delete</button> 
             </div>
-        `)
-    });
+            `)
+        }
+    );
+};
 
-    output.classList.toggle("hide");
+const deleteWorkout = (buttonEl) => {
+    const workoutArrIndex = workouts.findIndex((item) => item.id === buttonEl.parentElement.id);
+    buttonEl.parentElement.remove();
+    workouts.splice(workoutArrIndex, 1);
+    if (workouts.length < 1) {
+        workoutContainer.innerHTML = "";
+    }
+    localStorage.setItem("workouts", JSON.stringify(workouts));
+}
+
+const editWorkout = (buttonEl) => {
+    const workoutArrIndex = workouts.findIndex((item) => item.id === buttonEl.parentElement.id);
+
+    currentWorkout = workouts[workoutArrIndex];
+    movementName.value = currentWorkout.exercise;
+    setsInput.value = currentWorkout.sets;
+    repsInput.value = currentWorkout.reps;
+    weightInput.value = currentWorkout.weight;
+}
+
+addMovementButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    addOrUpdateWorkout();
 });
